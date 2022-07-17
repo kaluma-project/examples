19691eeb3073d922ceac73a82a93d4c2463b6f68
Tomás Correia Marques <tomasmanuelcorreiamarques@gmail.com>
2022-07-17 12:49:11 +0100

    make it actually blink

--- blink-pico-w/index.js
+++ blink-pico-w/index.js
@@ -1,7 +1,8 @@
-var led = 0
 const cyw43_arch = require('cyw43_arch');
+const led = 0
+let state = 1;
 
-cyw43_arch.gpioPut(led, 1);
 setInterval(() => {
-  cyw43_arch.gpioPut(led, 0);
+  cyw43_arch.gpioPut(led, state);
+  state = (state + 1 ) % 2; // Toggle between 1 and 0
 }, 1000);
